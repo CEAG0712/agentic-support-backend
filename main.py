ddbde370b97657a04c1143bd2112b3c84994b90f
Carlos Arosemena <carlosarosemena@macbookpro.home>
2025-08-06 18:44:50 -0400

    Initial typed API with async support

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from models import HealthResponse
 
 app = FastAPI()
 
-@app.get('/health')
-def read_root():
-    return {"message":"Scaffold Agentic Backend"}
+@app.get('/health', response_model=HealthResponse, tags=["health"])
+async def health() -> HealthResponse:
+    return HealthResponse(message="Agentic Backend Up and Running")
